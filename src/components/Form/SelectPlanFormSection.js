f548ba1a0b2113cc98ce985ad43876509c1a4faf
jakegodsall <jake.edward.godsall@gmail.com>
2023-01-11 09:21:44 +0000

    added state for slider

--- src/components/Form/SelectPlanFormSection.js
+++ src/components/Form/SelectPlanFormSection.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from '../UI/Slider';
 import PlanWidget from './FormWidgets/PlanWidget';
 
 import styles from './SelectPlanFormSection.module.css';
 
 const SelectPlanFormSection = () => {
+    const [sliderValue, setSliderValue] = useState(0);
+
+    const onChangeHandler = (value) => {
+        console.log(value);
+        if (value == 1) {
+            setSliderValue(true);
+        } else {
+            setSliderValue(false);
+        }
+    };
+
+    console.log(durationClasses);
+
     return (
         <div className={styles.formSection}>
             <h1>Select your plan</h1>
@@ -15,9 +28,9 @@ const SelectPlanFormSection = () => {
                 <PlanWidget id='pro' monthlyPrice={15}></PlanWidget>
             </div>
             <div className={styles.durationOptions}>
-                <p>Monthly</p>
-                <Slider></Slider>
-                <p>Yearly</p>
+                <p className={durationClasses}>Monthly</p>
+                <Slider onChange={onChangeHandler}></Slider>
+                <p className={durationClasses}>Yearly</p>
             </div>
         </div>
     );
--- src/components/UI/Slider.js
+++ src/components/UI/Slider.js
@@ -3,8 +3,20 @@ import React from 'react';
 import styles from './Slider.module.css';
 
 const Slider = (props) => {
+    const onChangeHandler = (e) => {
+        props.onChange(e.target.value);
+    };
+
     return (
-        <input className={styles.slider} id={props.labelID} type='range' min='0' max='1'></input>
+        <input
+            className={styles.slider}
+            id={props.labelID}
+            type='range'
+            min='0'
+            max='1'
+            defaultValue='0'
+            onChange={onChangeHandler}
+        ></input>
     );
 };
 
