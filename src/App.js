0cd2ea028a670787a4fc9834b0f93028d14ec209
jakegodsall <jake.edward.godsall@gmail.com>
2023-01-14 07:24:33 +0000

    added conditional rendering based on viewport

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import FormNav from './components/FormNav/FormNav';
 import Form from './components/Form/Form';
@@ -7,16 +7,36 @@ import styles from './App.module.css';
 
 const App = () => {
     const [step, setCurrentStep] = useState(1);
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth > 850);
+
+    useEffect(() => {
+        const updateMedia = () => {
+            setIsDesktop(window.innerWidth > 850);
+        };
+
+        window.addEventListener('resize', updateMedia);
+        return () => window.removeEventListener('resize', updateMedia);
+    });
 
     const onStepChange = (step) => {
         setCurrentStep(step);
     };
-    return (
+
+    const rendering = isDesktop ? (
         <main className={styles.container}>
             <FormNav currentStep={step}></FormNav>
             <Form stepChange={onStepChange}></Form>
         </main>
+    ) : (
+        <React.Fragment>
+            <FormNav currentStep={step}></FormNav>
+            <main className={styles.container}>
+                <Form stepChange={onStepChange}></Form>
+            </main>
+        </React.Fragment>
     );
+
+    return rendering;
 };
 
 export default App;
