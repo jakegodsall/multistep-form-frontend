157b89e1a646832bc46c38ca7d4ecd93120da27c
jakegodsall <jake.edward.godsall@gmail.com>
2023-01-17 06:10:47 +0000

    added the design to the thank you step

--- src/components/Form/ThankYouFormSection.js
+++ src/components/Form/ThankYouFormSection.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-import styled from './ThankYouFormSection.module.css';
+import styles from './ThankYouFormSection.module.css';
 
-const ThankYouFormSection = () => {};
+import ConfirmationIcon from '../../assets/images/icon-thank-you.svg';
+
+const ThankYouFormSection = () => {
+    return (
+        <div className={styles.formSection}>
+            <img src={ConfirmationIcon} alt='Thank You' />
+            <h1 className={styles.title}>Thank you!</h1>
+            <p className={styles.message}>
+                Thanks for confirming your subscription! We hope you have fun using our platform. If
+                you ever need support, pleasee feel free to email us at{' '}
+                <a href='mailto: support@loremgaming.com'>support@loremgaming.com</a>.
+            </p>
+        </div>
+    );
+};
 
 export default ThankYouFormSection;
